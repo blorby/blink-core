e56b55bd73eb13cdebbafbc150dd20dff3d469c0
Guy Manor <guy@blinkops.com>
2021-07-20 16:24:59 +0300

    Added default delimiter when fetching file

--- implementation/fetch-file-source/common.go
+++ implementation/fetch-file-source/common.go
@@ -11,10 +11,11 @@ import (
 )
 
 const (
-	fetchFileUrl         = "url"
-	fetchFileDestination = "destination"
-	pathDelimiter        = string(os.PathSeparator)
-	currentDir           = "./"
+	fetchFileUrl          = "url"
+	fetchFileDestination  = "destination"
+	pathDelimiter         = string(os.PathSeparator)
+	currentDir            = "./"
+	defaultParamDelimiter = "?"
 )
 
 func GetFileDestination(fileUrl string, request *plugin.ExecuteActionRequest, paramDelimiter string) (string, error) {
@@ -61,12 +62,12 @@ func GetFileUrl(request *plugin.ExecuteActionRequest) (string, error) {
 func extractFilenameFromUrl(fileUrl string, paramDelimiter string) string {
 	var fileName string
 
-	if paramDelimiter != "" {
-		splitUrl := strings.Split(fileUrl, paramDelimiter)
-		_, fileName = filepath.Split(splitUrl[0])
-	} else {
-		_, fileName = filepath.Split(fileUrl)
+	if paramDelimiter == "" {
+		paramDelimiter = defaultParamDelimiter
 	}
+
+	splitUrl := strings.Split(fileUrl, paramDelimiter)
+	_, fileName = filepath.Split(splitUrl[0])
 
 	return fileName
 }
