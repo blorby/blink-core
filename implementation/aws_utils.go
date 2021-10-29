303db0171ae3e5fc868b136140bd653efd6fcd90
Reshef Sharvit <subzero1122@gmail.com>
2021-10-29 18:46:20 +0300

    refactor

--- implementation/aws_utils.go
+++ implementation/aws_utils.go
@@ -7,9 +7,9 @@ import (
 	"github.com/aws/aws-sdk-go/service/sts"
 	log "github.com/sirupsen/logrus"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"strconv"
-	"math/rand"
 )
 
 
@@ -42,6 +42,51 @@ func convertInterfaceMapToStringMap(m map[string]interface{}) map[string]string 
 	return mapString
 }
 
+func readFile(path string) (string, error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
+}
+
+func assumeRoleWithWebIdentity(svc *sts.STS, role, sessionName string) (string, string, string, error) {
+	log.Debug("assuming role with web identity")
+	tokenFile, ok := os.LookupEnv("AWS_WEB_IDENTITY_TOKEN_FILE")
+	if !ok {
+		return "", "", "", fmt.Errorf("token file for irsa not found. make sure your pod is configured correctly and that your service account is created and annotated properly")
+	}
+
+	data, err := readFile(tokenFile)
+	if err != nil {
+		return "", "", "", fmt.Errorf("unable to open web identity token file with error: %w", err)
+	}
+
+	result, err := svc.AssumeRoleWithWebIdentity(&sts.AssumeRoleWithWebIdentityInput{
+		DurationSeconds:  aws.Int64(3600),
+		RoleArn:          aws.String(role),
+		RoleSessionName:  aws.String(sessionName),
+		WebIdentityToken: aws.String(string(data)),
+	})
+	if err != nil {
+		return "", "", "", err
+	}
+	return *result.Credentials.AccessKeyId, *result.Credentials.SecretAccessKey, *result.Credentials.SessionToken, err
+}
+
+func assumeRoleWithTrustedIdentity(svc *sts.STS, role, externalID, sessionName string) (string, string, string, error) {
+	log.Debug("assuming role with trusted entity")
+	result, err := svc.AssumeRole(&sts.AssumeRoleInput{
+		RoleArn:         &role,
+		RoleSessionName: &sessionName,
+		ExternalId:      &externalID,
+	})
+	if err != nil {
+		return "", "", "", err
+	}
+	return *result.Credentials.AccessKeyId, *result.Credentials.SecretAccessKey, *result.Credentials.SessionToken, err
+}
+
 func assumeRole(role, externalID, region string) (access, secret, sessionToken string, err error) {
 	sess, _ := session.NewSession(&aws.Config{
 		Region: aws.String(region),
@@ -52,38 +97,7 @@ func assumeRole(role, externalID, region string) (access, secret, sessionToken s
 
 	// irsa does not work with externalID, only the "traditional" assume role does
 	if externalID == "" {
-		tokenFile, ok := os.LookupEnv("AWS_WEB_IDENTITY_TOKEN_FILE")
-		if !ok {
-			return access, secret, sessionToken, fmt.Errorf("token file for irsa not found. make sure pod is configured correctly and that your service account is created and properly annotated")
-		}
-
-		log.Debug("assuming role with web identity")
-		data, err := ioutil.ReadFile(tokenFile)
-		if err != nil {
-			return access, secret, sessionToken, fmt.Errorf("unable to open web identity token file with error: %w", err)
-		}
-
-		result, err := svc.AssumeRoleWithWebIdentity(&sts.AssumeRoleWithWebIdentityInput{
-			DurationSeconds:  aws.Int64(3600),
-			RoleArn:          aws.String(role),
-			RoleSessionName:  aws.String(sessionName),
-			WebIdentityToken: aws.String(string(data)),
-		})
-		if err != nil {
-			return access, secret, sessionToken, fmt.Errorf("unable to assume web identity role with error: %w", err)
-		}
-		access, secret, sessionToken = *result.Credentials.AccessKeyId, *result.Credentials.SecretAccessKey, *result.Credentials.SessionToken
-	} else {
-		log.Debug("assuming role with trusted entity")
-		result, err := svc.AssumeRole(&sts.AssumeRoleInput{
-			RoleArn:         &role,
-			RoleSessionName: &sessionName,
-			ExternalId:      &externalID,
-		})
-		if err != nil {
-			return access, secret, sessionToken, fmt.Errorf("unable to assume trusted identity role with error: %w", err)
-		}
-		access, secret, sessionToken = *result.Credentials.AccessKeyId, *result.Credentials.SecretAccessKey, *result.Credentials.SessionToken
+		return assumeRoleWithWebIdentity(svc, role, sessionName)
 	}
-	return access, secret, sessionToken, nil
+	return assumeRoleWithTrustedIdentity(svc, role, externalID, sessionName)
 }
