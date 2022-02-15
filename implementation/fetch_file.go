636318a41c96ab1fc3ab5294bdcbf9c597593b8f
Yakir Oren <yakiroren@gmail.com>
2022-02-15 11:21:01 +0200

    CU-27ctx79: Add Show Output to fetch file action. (#72)

--- implementation/fetch_file.go
+++ implementation/fetch_file.go
@@ -7,9 +7,26 @@ import (
 	"github.com/blinkops/blink-core/implementation/fetch-file-source/gitlab"
 	"github.com/blinkops/blink-sdk/plugin"
 	log "github.com/sirupsen/logrus"
+	"os"
+	"strconv"
 )
 
 func executeCoreFetchFileAction(e *execution.PrivateExecutionEnvironment, ctx *plugin.ActionContext, request *plugin.ExecuteActionRequest) ([]byte, error) {
+	filePath, err := fetchFile(e, ctx, request)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	if showOutput, ok := request.Parameters["show_output"]; ok {
+		if val, _ := strconv.ParseBool(showOutput); val {
+			return os.ReadFile(string(filePath))
+		}
+	}
+
+	return filePath, nil
+}
+
+func fetchFile(e *execution.PrivateExecutionEnvironment, ctx *plugin.ActionContext, request *plugin.ExecuteActionRequest) ([]byte, error) {
 	if github.CheckForConnection(ctx) {
 		log.Infof("Fetching file from GitHub")
 		return github.FetchFile(e, ctx, request)
